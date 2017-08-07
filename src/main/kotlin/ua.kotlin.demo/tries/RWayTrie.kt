611a1e7244d33581cd6616c85859cae165a99034
leveret <leveretka@github.com>
2017-08-07 04:26:40 +0300

    Rewrite add method appropriate to algorithm and refactoring of contains method

--- src/main/kotlin/ua.kotlin.demo/tries/RWayTrie.kt
+++ src/main/kotlin/ua.kotlin.demo/tries/RWayTrie.kt
@@ -5,21 +5,36 @@ private val ALPHABET_SIZE = 26
 class RWayTrie : Trie {
 
     var size = 0
+    val root = Node()
 
-    data class Node(val value: Object? = null, var nodes: Array<Node?> = Array<Node?>(ALPHABET_SIZE, {null}))
+    data class Node(var value: Int? = null, var nodes: Array<Node?> = Array<Node?>(ALPHABET_SIZE, {null}))
 
-    val words = ArrayList<String>();
+    override fun add(word: Pair<String, Int?>) {
+        val key = word.first
+        var x = root
+        for (c in key.toCharArray()) {
+            if (x.nodes[c - 'a'] == null)
+                x.nodes[c - 'a'] = Node()
 
-    override fun add(word: Pair<String, Int?>?) {
-        words.add(word!!.first)
+            x = x.nodes[c -  'a'] as Node
+        }
+        x.value = word.second
+
         size++
     }
 
-    override fun contains(word: String?): Boolean {
-        return words.contains(word)
+    override fun contains(word: String): Boolean {
+        var x = root
+        for (c in word.toCharArray()) {
+            if (x.nodes[c - 'a'] == null)
+                return false
+
+            x = x.nodes[c -  'a'] as Node
+        }
+        return x.value != null
     }
 
-    override fun delete(word: String?): Boolean {
+    override fun delete(word: String): Boolean {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -27,7 +42,7 @@ class RWayTrie : Trie {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun wordsWithPrefix(pref: String?): MutableIterable<String> {
+    override fun wordsWithPrefix(pref: String): MutableIterable<String> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
--- src/test/kotlin/ua/kotlin/demo/tries/RWayTrieTest.kt
+++ src/test/kotlin/ua/kotlin/demo/tries/RWayTrieTest.kt
@@ -31,4 +31,28 @@ class RWayTrieTest {
         rWayTrie.add("a" to 1)
         assertFalse (rWayTrie.contains("aa"))
     }
+
+    @Test
+    fun shouldAddTwoWordsAtOneLevel() {
+        rWayTrie.add("a" to 1)
+        rWayTrie.add("b" to 1)
+
+        val firstWord = rWayTrie.root.nodes[0]
+        assertEquals(1, firstWord?.value)
+
+        val secondWord = rWayTrie.root.nodes[1]
+        assertEquals(1, secondWord?.value)
+    }
+
+    @Test
+    fun shouldAddTwoWordsAtDifferentLevels() {
+        rWayTrie.add("a" to 1)
+        rWayTrie.add("ab" to 2)
+
+        val firstWord = rWayTrie.root.nodes[0]
+        assertEquals(1, firstWord?.value)
+
+        val secondWord = firstWord?.nodes?.get(1)
+        assertEquals(2, secondWord?.value)
+    }
 }
